551e866f36f02260172b586380e714675dd8758e
LucasMeyble <lucasmeyble123@gmail.com>
2024-04-15 21:49:00 -0300

    fix: ajeitando resposividade

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex justify-center items-center">
-    <div class="text-lg text-center mt-10 mx-20 lg:mt-24 lg:mx-40 px-20 ls:px-40 py-20 border-solid border-2 rounded-xl bg-gray-300 border-gray-600 space-y-8 lg:space-y-10">
+    <div class="text-lg text-center mt-10 mx-20 lg:mt-24 lg:mx-40 px-20 ls:px-40 py-4 lg:py-20 border-solid border-2 rounded-xl bg-gray-300 border-gray-600 space-y-8 lg:space-y-10">
       <p>This project consists of resolving all projects that were created in the github repository: <a href="https://github.com/florinpop17/app-ideas" target="_blank" class="text-yellow-700 italic font-bold">app-ideas.</a></p>
       <p>The following technologies were used in this project: vite, vue3, pinia, tailwind.</p>
     </div>
